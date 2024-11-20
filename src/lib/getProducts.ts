f70a1532fc499e77cfc1ed58481edc82aa4e0df1
riiz08 <mhmdrizki0812@gmail.com>
2024-11-20 08:35:18 +0700

    alhamdulillah lancar

--- src/lib/getProducts.ts
+++ src/lib/getProducts.ts
@@ -1,5 +1,3 @@
-import { dummyProducts } from "./dummyData";
-
 interface ProductData {
   name: string;
   image: string;
@@ -10,20 +8,16 @@ interface ProductData {
 
 export const getAllParfum = async () => {
   try {
-    if (process.env.NODE_ENV === "production") {
-      const fetching = await fetch(
-        `${process.env.API_BASE_URL}/api/v1/produk/parfum`
-      );
+    const fetching = await fetch(
+      `${process.env.API_BASE_URL}/api/v1/produk/parfum`
+    );
 
-      if (!fetching.ok) {
-        throw new Error(`HTTP error! Status: ${fetching.status}`);
-      }
-
-      const response = await fetching.json();
-      return response.data;
-    } else {
-      return dummyProducts;
+    if (!fetching.ok) {
+      throw new Error(`HTTP error! Status: ${fetching.status}`);
     }
+
+    const response = await fetching.json();
+    return response.data;
   } catch (error) {
     console.error("Error fetching fashion wanita:", error);
     return []; // Kembalikan array kosong jika terjadi error
@@ -32,20 +26,16 @@ export const getAllParfum = async () => {
 
 export const getAllFashionPria = async () => {
   try {
-    if (process.env.NODE_ENV === "production") {
-      const fetching = await fetch(
-        `${process.env.API_BASE_URL}/api/v1/produk/fashion-pria`
-      );
+    const fetching = await fetch(
+      `${process.env.API_BASE_URL}/api/v1/produk/fashion-pria`
+    );
 
-      if (!fetching.ok) {
-        throw new Error(`HTTP error! Status: ${fetching.status}`);
-      }
-
-      const response = await fetching.json();
-      return response.data;
-    } else {
-      return dummyProducts;
+    if (!fetching.ok) {
+      throw new Error(`HTTP error! Status: ${fetching.status}`);
     }
+
+    const response = await fetching.json();
+    return response.data;
   } catch (error) {
     console.error("Error fetching fashion wanita:", error);
     return []; // Kembalikan array kosong jika terjadi error
@@ -54,20 +44,16 @@ export const getAllFashionPria = async () => {
 
 export const getAllFashionWanita = async () => {
   try {
-    if (process.env.NODE_ENV === "production") {
-      const fetching = await fetch(
-        `${process.env.API_BASE_URL}/api/v1/produk/fashion-wanita`
-      );
+    const fetching = await fetch(
+      `${process.env.API_BASE_URL}/api/v1/produk/fashion-wanita`
+    );
 
-      if (!fetching.ok) {
-        throw new Error(`HTTP error! Status: ${fetching.status}`);
-      }
-
-      const response = await fetching.json();
-      return response.data;
-    } else {
-      return dummyProducts;
+    if (!fetching.ok) {
+      throw new Error(`HTTP error! Status: ${fetching.status}`);
     }
+
+    const response = await fetching.json();
+    return response.data;
   } catch (error) {
     console.error("Error fetching fashion wanita:", error);
     return []; // Kembalikan array kosong jika terjadi error
